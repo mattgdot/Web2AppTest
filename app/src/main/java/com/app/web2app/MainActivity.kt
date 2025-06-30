fc9763a8ace2792dfc45c15ddc89a94a99fd5595
Matthew G. <matei.grejdan@gmail.com>
2025-06-30 14:07:47 +0300

    upd

--- app/src/main/java/com/app/web2app/MainActivity.kt
+++ app/src/main/java/com/app/web2app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.app.web2app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,11 +14,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.app.web2app.ui.theme.Web2AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             Web2AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
